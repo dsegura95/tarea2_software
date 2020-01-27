d7b24d5fed0791f3ed80a812458ad9906f388801
Jesus Kauze <12-10273@usb.ve>
2020-01-27 17:25:44 -0500

    Funcion para calcular edad

--- pension.py
+++ pension.py
@@ -16,6 +16,32 @@
 # antes de tiempo, se rebaja un año por cada cuatro trabajados un maximo
 # de 5 años
 
-
+# Imports
+import datetime
 
 # Manejo de fechas mediante liberia de python
+
+
+# Funcion para calcular la edad, Parametros: Fecha de nacimiento
+
+def CalcularEdad(nacimiento):
+    hoy = datetime.date.today()
+
+    # Edad Negativa por fecha futura
+    if hoy < nacimiento:
+        print('Fecha de nacimiento Invalida')
+    elif (hoy.year - nacimiento.year)  > 140:
+        print('La Fecha proporcionada supera los 140 anos de edad, Por favor verifique e ingrese nuevamente')
+    else:
+        ano = nacimiento.year
+        mes = nacimiento.month
+        dia = nacimiento.day
+        fecha = nacimiento
+        edad = 0
+        while fecha < hoy:
+            edad += 1
+            fecha = datetime.date(ano+edad, mes, dia)
+
+        # print('Mi edad es: %s' % (edad-1))
+        return edad-1
+
