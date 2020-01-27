042fada4719752e29d1a7487d04138b22f5ecb36
David Segura <dsegura95@hotmail.com>
2020-01-27 16:41:36 -0400

    Creacion de archivo pension.py

---
+++ pension.py
@@ -0,0 +1,21 @@
+# PENSION JK & D10S
+
+# Función que devuelva como resultado si una persona cumple con
+# los requisitos legales para recibir una pensión de vejez del
+# Instituto Venezolano de los Seguros Sociales (IVSS).
+
+# AUTORES:
+
+# David Segura #13-11341
+# Jesus Kauze #12-10273
+
+# Funcion que calcule segun edad >= 60 si es H, >= 55 si es mujer,
+# acreditadas un minimo de 750 semanas cotizadas
+
+# Funcion que calcule insalubridad, te da derecho a cobrar la pension
+# antes de tiempo, se rebaja un año por cada cuatro trabajados un maximo
+# de 5 años
+
+
+
+# Manejo de fechas mediante liberia de python
