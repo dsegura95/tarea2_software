59b0fd618887b6682427f408fe8b8a846b4a4962
David Segura <dsegura95@hotmail.com>
2020-01-27 18:28:55 -0400

    Quitando comentarios inutiles

--- pension.py
+++ pension.py
@@ -9,15 +9,6 @@
 # David Segura #13-11341
 # Jesus Kauze #12-10273
 
-# Funcion que calcule segun edad >= 60 si es H, >= 55 si es mujer,
-# acreditadas un minimo de 750 semanas cotizadas
-
-# Funcion que calcule insalubridad, te da derecho a cobrar la pension
-# antes de tiempo, se rebaja un año por cada cuatro trabajados un maximo
-# de 5 años
-
-# Manejo de fechas mediante liberia de python
-
 def pensionado(sexo,edad,semanas,salubridad):
     limitehombre = 60
     limitemujer = 55
